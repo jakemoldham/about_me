f2afc9348dbc008f0b0fa3a236f3eafe04553b79
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-07-17 14:34:41 -0600

    added email settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'morning-brushlands-2457.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV['EMAIL_DOMAIN'],
+    user_name:            ENV['EMAIL_USER_NAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -76,5 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { :host => 'morning-brushlands-2457.herokuapp.com' }
 end
