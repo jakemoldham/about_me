cdc82d8561da69d059635bbc7632fca6090aed1f
Jake Oldham <jakemoldham@gmail.com>
2016-12-16 00:02:09 -0700

    fixed issue with deleting dependents on questions and answers. Setup only one choice js for correct answer

--- app/models/to_live/question.rb
+++ app/models/to_live/question.rb
@@ -1,4 +1,4 @@
 class ToLive::Question < ActiveRecord::Base
-  has_many :answers
-  accepts_nested_attributes_for :answers
+  has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, allow_destroy: true
 end
