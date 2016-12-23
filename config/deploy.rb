e18d7e4dcd602f0caa32656576287a52db91635b
jakemoldham <jakemoldham@gmail.com>
2016-12-23 11:26:36 -0700

    removed set scm git from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,14 @@ set :branch, :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :scm, :pretty
 set :deploy_to, '~/www/jakeoldham.com'
 set :linked_dirs, %w{log tmp vendor/bundle public/system public/uploads}
 set :rails_env, "production"
 
 # Default value for :scm is :git
-set :scm, :git
+# This has been depriciated
+# set :scm, :git
+# set :scm, :pretty
 
 # Default value for :format is :pretty
 set :format, :pretty
